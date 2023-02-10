6d70179f3546e5e70dfe0de051e2eb3a36f274fd
JORGE LUIS DE FERRARI <joruge@gmail.com>
2023-02-10 19:26:01 -0300

    29. Classes and Namespaces

---
+++ CSharpPractice.sln/Classes/BankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice.sln.Classes
+{
+    public  class BankAccount
+    {
+        public double AddToBalance(double balanceToBeAdded)
+        {
+            return balanceToBeAdded;
+        }
+    }
+}
---
+++ CSharpPractice.sln/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharpPractice.sln
+{
+    internal class Program
+    {
+        static double numberTwo = 12.34;
+
+        static void Main(string[] args)
+        {
+
+            if(numberTwo == 1)
+            {
+                var numberThree = 12f;
+            }
+            int numberOne = 23;
+            Console.WriteLine($"{numberOne}");
+            Console.WriteLine($"{numberTwo}");
+            var message = Console.ReadLine();
+            Console.WriteLine(SimpleMath.Add(numberOne, numberTwo));
+            Console.ReadLine();
+        }
+    }
+
+    class SimpleMath
+    {
+        public static double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
