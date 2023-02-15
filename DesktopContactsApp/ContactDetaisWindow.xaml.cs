83e5348dafc3291db1860e9826032afa21327c15
JORGE FERRARI <joruge@gmail.com>
2023-02-14 23:49:46 -0300

    Delete

---
+++ DesktopContactsApp/ContactDetaisWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using DesktopContactsApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for ContactDetaisWindow.xaml
+    /// </summary>
+    public partial class ContactDetaisWindow : Window
+    {
+
+        Contacts contact; 
+        public ContactDetaisWindow(Contacts contact)
+        {
+            InitializeComponent();
+            this.contact = contact;
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
