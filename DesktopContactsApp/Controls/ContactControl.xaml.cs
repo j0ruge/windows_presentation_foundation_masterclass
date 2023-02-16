c0ea549386b2109cf6cf5e347cab03711fbefd59
JORGE FERRARI <joruge@gmail.com>
2023-02-15 22:31:56 -0300

    Custon

---
+++ DesktopContactsApp/Controls/ContactControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using DesktopContactsApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp.Controls
+{
+    /// <summary>
+    /// Interaction logic for ContactControl.xaml
+    /// </summary>
+    public partial class ContactControl : UserControl
+    {
+
+        
+        private Contacts contact;
+        
+        public Contacts Contact
+        {
+            get { return contact; }
+            set { 
+                contact = value;
+                nameTextBlock.Text = contact.Name;
+                emailTextBlock.Text = contact.Email;
+                phoneTextBlock.Text = contact.Phone;    
+            
+            }
+        }
+
+
+        public ContactControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
