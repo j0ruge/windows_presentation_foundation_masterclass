ca3c4f292349eb3daa57eaadc2bc4f1f708f4e0c
JORGE LUIS DE FERRARI <joruge@gmail.com>
2023-03-03 20:34:15 -0300

    EverNOte Clone

---
+++ EvernoteClone/ViewModel/Helpers/DatabaseHelper.cs
@@ -0,0 +1,77 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernoteClone.ViewModel.Helpers
+{
+    public class DatabaseHelper
+    {
+        private static string dbFile = Path.Combine(Environment.CurrentDirectory, "notesDb.db3");
+
+        public static bool Insert<T>(T item)
+        {
+            bool result = false;
+
+            using (SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<T>();
+                int rows = connection.Insert(item);
+                if(rows > 0)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        public static bool Update<T>(T item)
+        {
+            bool result = false;
+
+            using (SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<T>();
+                int rows = connection.Update(item);
+                if (rows > 0)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        public static bool Delete<T>(T item)
+        {
+            bool result = false;
+
+            using (SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<T>();
+                int rows = connection.Delete(item);
+                if (rows > 0)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        public static List<T> Read<T>(T item) where T : new()
+        {
+            List<T> items;
+
+            using (SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<T>();
+                items = connection.Table<T>().ToList();
+               
+            }
+            return items;
+        }
+
+    }
+}
